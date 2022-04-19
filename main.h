711f8c5c4067ee3eb8dc5394dfb2adae777d0618
manassehkim <49023146+manassehkim@users.noreply.github.com>
2022-04-19 19:35:35 +0300

    print c file upload

--- main.h
+++ main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *format );
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+char *print_s(va_list list);
+char *print_c(va_list list);
+char *print_d(va_list list);
+
 
